ca2461bb790ebfff98e2931e74f6ef8e87f17854
ZoubairMabrouk <zoubairmabrouk55@gmail.com>
2025-06-11 16:48:19 +0100

    PCC commit

--- src/strategy_pcc/StrategyPCC.java
+++ src/strategy_pcc/StrategyPCC.java
@@ -1,15 +1,4 @@
 package strategy_pcc;
 
-import model.GraphNode;
-
-import java.util.List;
-
 public class StrategyPCC {
-    private PCC_Srategy pccSrategy;
-    public void setPccSrategy(PCC_Srategy pccSrategy){
-        this.pccSrategy = pccSrategy;
-    }
-    public List<GraphNode> caluculate(){
-       return pccSrategy.pcc();
-    }
 }
